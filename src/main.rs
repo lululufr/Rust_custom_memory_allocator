bbe1499ca0214364437b9c31e949dee2565984aa
lululufr <lucas.lm.miller@gmail.com>
2024-10-12 16:08:31 +0200

    Adding

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,66 @@
 
 use core::panic::PanicInfo;
 
-
 #[panic_handler]
-fn panic(_info : &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-
 use core::alloc::{GlobalAlloc, Layout};
 use core::arch::asm;
+use core::ptr::null_mut;
+use core::u8;
 
-pub struct GreatAllocator;
+pub struct Lululucator {
+    heap_start: usize,
+    heap_end: usize,
+    init: bool,
+    // free_list: *mut FreeBlock,
+}
 
-unsafe impl GlobalAlloc for GreatAllocator {
+unsafe impl GlobalAlloc for Lululucator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-
-        let brk_addr: usize;
+        let mut brk_addr: usize;
 
         asm!(
-        "mov rax, 0xc",
-        "mov rdi, 0",
-        "syscall",
-        
-        "mov rdi, rax",
-        "mov rax, 0xc",
-        "add rdi, 1000",
-        "syscall",
-        lateout("rax") brk_addr,
+            "mov rax, 12",
+            "mov rdi, 0",
+            "syscall",
+            "mov rdi, rax",
+
+            "add rdi, {size}",
+            "mov rax, 12",
+            "syscall",
+
+            size = in(reg) layout.size(),
+            lateout("rax") brk_addr,
+            out("rdi") _,
         );
 
-        brk_addr as *mut u8
+        (brk_addr - layout.size()) as *mut u8
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        //do shit
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
 
+impl Lululucator {
+    const fn new() -> Lululucator {
+        Lululucator {
+            heap_start: 0,
+            heap_end: 0,
+            init: false,
+        }
     }
 }
 
-
 #[global_allocator]
-static ALLOCATOR: GreatAllocator = GreatAllocator;
+static ALLOCATOR: Lululucator = Lululucator::new();
 
 #[no_mangle]
-unsafe extern "C" fn _start() -> usize{
+pub extern "C" fn _start() -> ! {
+    let layout = Layout::from_size_align(1000, 1).unwrap();
 
-    let addr = ALLOCATOR.alloc(Layout::new::<u8>());
+    let maVariable = unsafe { ALLOCATOR.alloc(layout) };
 
-    0
-
-
-
-
+    loop {}
 }
